7954636e75fef40e5a03b07016236a723ae5ca79
Cristian Rojas <cristianguillermorojas@gmail.com>
2013-06-27 08:54:11 -0500

    Doing changes...

--- library/js/aseguradoras-en-colombia.js
+++ library/js/aseguradoras-en-colombia.js
@@ -3,4 +3,22 @@ jQuery(document).ready(function($) {
     $('a:contains("Aseguradoras en Colombia")').parent().attr('id', 'aseguradoras');
 
     $('li#aseguradoras > ul.children').attr('id', 'main-children');
+
+    $('ul#main-children > li').each(function(){
+        var self = $(this);
+        self.children('a').addClass('collapse');
+    });
+
+    $('ul#main-children').on('click', 'a.collapse', function(e){
+        e.preventDefault();
+
+        var self = $(this);
+        self.toggleClass('open');
+
+        if( self.hasClass('open') ){
+            self.closest('ul.children').show();
+        } else {
+            self.closest('ul.children').hide();
+        }
+    });
 });
