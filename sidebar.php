bc5791225aaeae7a7c0bbe3427af42d390a0fe00
Cristian Rojas <cristianguillermorojas@gmail.com>
2013-06-27 09:33:48 -0500

    Doing changes...

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,13 @@
 <div id="sidebar1" class="fluid-sidebar sidebar span4" role="complementary">
 
     <div id="quote-now-button" class="widget widget-quote-button clearfix">
-        <h3 class="nm">Seguros para Vehículos</h3>
-        <h4 class="nmt">Cotiza en línea tu seguro todo riesgo</h4>
-        <div class="row-fluid">
-            <ul class="unstyled">
-                <li class="no-margin"><i class="icon-ok icon-2x orange"></i><span class="lead">Cotiza en menos de <strong>5 minutos!</strong></span></li>
-                <li class="no-margin"><i class="icon-ok icon-2x orange"></i><span class="lead">Compara precios y coberturas</span></li>
-                <li class="no-margin"><i class="icon-ok icon-2x orange"></i><span class="lead">Solicita tu póliza desde tu casa</span></li>
-            </ul>
-        </div>
+        <h3 class="nmt mb10 center">Seguros para Vehículos</h3>
+        <h4 class="nmt mb20 center">Cotiza en línea tu seguro todo riesgo</h4>
+        <ul class="unstyled mb20">
+            <li class="no-margin"><i class="icon-ok icon-2x orange"></i><span class="lead">Cotiza en menos de <strong>5 minutos!</strong></span></li>
+            <li class="no-margin"><i class="icon-ok icon-2x orange"></i><span class="lead">Compara precios y coberturas</span></li>
+            <li class="no-margin"><i class="icon-ok icon-2x orange"></i><span class="lead">Solicita tu póliza desde tu casa</span></li>
+        </ul>
         <div class="row-fluid">
             <a href="/seguros-para-vehiculos/aplicar/" class="btn btn-primary btn-large">Cotizar en línea</a>
         </div>
