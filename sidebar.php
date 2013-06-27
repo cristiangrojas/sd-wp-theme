b6750bf8b81ab89f5436f793a9fbd93ae8c52aa9
Cristian Rojas <cristianguillermorojas@gmail.com>
2013-06-27 09:19:15 -0500

    Doing changes...

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,38 @@
-				<div id="sidebar1" class="fluid-sidebar sidebar span4" role="complementary">
-				
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+<div id="sidebar1" class="fluid-sidebar sidebar span4" role="complementary">
 
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
+    <div id="quote-now-button" class="widget widget-quote-button">
+        <div class="span6 text-top">
+            <div class="row-fluid">
+                <h3 class="no-margin">Seguros para Vehículos</h3>
+                <h4 class="no-margin-top">Cotiza en línea tu seguro todo riesgo</h4>
+            </div>
+            <div class="row-fluid">
+                <ul class="unstyled">
+                    <li class="no-margin"><i class="icon-ok icon-2x"></i><span class="lead">Cotiza en menos de <strong>5 minutos!</strong></span></li>
+                    <li class="no-margin"><i class="icon-ok icon-2x"></i><span class="lead">Compara precios y coberturas</span></li>
+                    <li class="no-margin"><i class="icon-ok icon-2x"></i><span class="lead">Solicita tu póliza desde tu casa</span></li>
+                </ul>
+            </div>
+            <div class="row-fluid">
+                <a href="/seguros-para-vehiculos/aplicar/" class="btn btn-primary btn-large">Cotizar en línea</a>
+            </div>
+        </div>
+    </div>
 
-					<?php else : ?>
+    <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
-						<!-- This content shows up if there are no widgets defined in the backend. -->
-						
-						<div class="alert alert-message">
-						
-							<p><?php _e("Please activate some Widgets","bonestheme"); ?>.</p>
-						
-						</div>
+        <?php dynamic_sidebar( 'sidebar1' ); ?>
 
-					<?php endif; ?>
+    <?php else : ?>
 
-				</div>
+        <!-- This content shows up if there are no widgets defined in the backend. -->
+
+        <div class="alert alert-message">
+
+            <p><?php _e("Please activate some Widgets","bonestheme"); ?>.</p>
+
+        </div>
+
+    <?php endif; ?>
+
+</div>
