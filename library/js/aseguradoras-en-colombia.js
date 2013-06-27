76c51aa140b53df55bebee16215b9469c8468be3
Cristian Rojas <cristianguillermorojas@gmail.com>
2013-06-27 08:49:51 -0500

    Doing changes...

--- library/js/aseguradoras-en-colombia.js
+++ library/js/aseguradoras-en-colombia.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($) {
     // Get a element with "Aseguradoras en Colombia" text and add an id to the "li" parent
     $('a:contains("Aseguradoras en Colombia")').parent().attr('id', 'aseguradoras');
+
+    $('li#aseguradoras > ul.children').attr('id', 'main-children');
 });
